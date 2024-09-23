b238ae582a90459b3218c4cad1af937eabdf8c0e
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2024-09-23 10:56:35 -0400

    Update prolific_study.js

--- js/prolific_study.js
+++ js/prolific_study.js
@@ -690,6 +690,7 @@ function init() {
   exp.catch_trials = [];
   exp.consent = [];
   var stimuli = all_stims;
+  
   var list_index = parseInt(get_url_param("list", 0)); // Get list param, default to 0
   exp.stimuli = _.shuffle(stimuli[list_index]);
 
@@ -732,6 +733,7 @@ function init() {
     "subj_info",
     "thanks"
   ];
+
   exp.data_trials = [];
 
   //make corresponding slides:
